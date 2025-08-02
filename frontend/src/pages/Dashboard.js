c4e6d1865900658eddbe4a30b3bb7d4295e4ba89
SurajKothule <153737977+SurajKothule@users.noreply.github.com>
2025-08-03 01:39:47 +0530

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -13,7 +13,7 @@ export default function Dashboard() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/predict", {
+      const response = await fetch("https://ai-study-planner-t3np.onrender.com/api/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
