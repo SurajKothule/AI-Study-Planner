3707b41f18897baef3d89c1d78a675c4c9ca9395
SurajKothule <153737977+SurajKothule@users.noreply.github.com>
2025-08-03 01:40:37 +0530

    Update Predictor.js

--- frontend/src/pages/Predictor.js
+++ frontend/src/pages/Predictor.js
@@ -14,7 +14,7 @@ export default function Predictor() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/predict", {
+      const response = await fetch("https://ai-study-planner-t3np.onrender.com/api/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
